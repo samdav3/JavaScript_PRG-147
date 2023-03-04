760d22dac5b9d2842b523a52ea689997cf8ee0ae
Sam Davenport <sdavenport@students.mchenry.edu>
2023-03-03 23:15:26 -0600

    initial commit

---
+++ learn13-Completed/objects-assessment-program/scripts/scripts.js
@@ -0,0 +1,190 @@
+const recipe1 = {
+    name: "Simple Chicken Pho",
+    category: "Dinner",
+    image: "images/pho.png",
+    ingredients: "½ tablespoon sesame oil,\
+        3 star anise, 2 garlic cloves minced,\
+        1 teaspoon grated ginger or ginger paste,\
+        6 cups chicken stock,\
+        3 tablespoons low-sodium soy sauce,\
+        2-3 tablespoons hoisin sauce,\
+        1 teaspoon fish sauce, juice from 1 lime,\
+        1 jalapeño pepper, \
+        sliced 1 rotisserie chicken, \
+        thinly sliced or shredded 8 ounces flat rice noodles. \
+        Optional toppings:\
+        lime wedges,\
+        cilantro, \
+        Thai basil, \
+        bean sprouts",
+    directions: "1. Heat the sesame oil in a large pot set over medium heat. Add the star anise, garlic, and ginger; heat until fragrant,\
+        about 30-60 seconds. \
+        2. Pour in the stock, soy sauce, hoisin sauce, fish sauce, lime juice, jalapeño pepper, and chicken. \
+        Bring to a boil, then reduce the heat to low and simmer for 15-20 minutes uncovered.\
+        3. Cook the noodles according to the package instructions. Divide them between 4 bowls and ladle the soup over the noodles.\
+        4. Garnish with lime, cilantro, Thai basil, and bean sprouts as desired.",
+    source: "https://jz-eats.com/chicken-pho/"
+}
+const recipe2 = {
+    name: "Baked Chicken Parm",
+    category: "Dinner",
+    image: "images/chickparm.png",
+    ingredients: "3 pieces boneless, \
+        skinless chicken breast, \
+        2 eggs , whisked, \
+        ½ teaspoon Kosher salt,\
+        ¼ teaspoon pepper, \
+        1 cup Panko or Italian bread crumbs, \
+        ½ cup grated parmesan cheese, \
+        1 teaspoon garlic powder,\
+        2 cups marinara or spaghetti sauce, \
+        6-8 ounces fresh mozzarella , \
+        sliced about 1/8 inch thick",
+    directions: "1. Preheat oven to 415°F. Spray a baking sheet or jelly roll pan with non-stick cooking spray and set aside. \
+        2. Cut chicken in half horizontally to create 6 fillets. Place in a zip top bag and flatten with a meat tenderizer or \
+        rolling pin to about 1/2 inch thick. \
+        3. Place egg in a bowl and whisk together with salt and pepper. Place bread crumbs, parmesan cheese and garlic powder in a separate bowl.\
+        Dip chicken in the egg and then place in the bread crumbs and cover the chicken.\
+        4. Next place chicken in the pan and bake for 20-25 minutes or until it's no longer pink in the middle. Flip the chicken half way through. \
+        Remove from oven and spoon about 1/4 cup sauce on top of the chicken and top with a slice of mozzarella cheese. Bake for an additional 3-5\
+        minutes or until cheese is melted. Broil the last minute if desired. \
+        5. Top with fresh basil or oregano. Serve with angel hair pasta or spaghetti if desired. Enjoy!",
+    source: "https://www.pinterest.com/pin/572097958918435478/",
+}
+const recipe3 = {
+    name: "Cowboy Turkey Burgers & Ranch Slaw",
+    category: "Dinner",
+    image: "images/burger.png",
+    ingredients: "Burger:\
+        1 1/4 Lbs. Extra Lean Ground Turkey Breast,\
+        2/3 C. Carrot, Grated,\
+        2 Tbsp. Whole Wheat Bread Crumbs,\
+        2 Tbsp. Red Onion, Diced,\
+        2 Tbsp. Cilantro, Chopped,\
+        2 Tbsp. Bacon, Cooked & Crumbled,\
+        1 Tbsp. Hot Sauce (Tobasco),\
+        1 Egg White,\
+        1-2 Jalapeño, Seeded, Stemed & Diced,\
+        1 Clove Garlic, Minced,\
+        Salt & Pepper To Taste,\
+        2 1/2 Oz. Sharp Cheddar, Crumbled or Diced Small Cubes,\
+        Slaw:\
+        2 Tbsp. Fat Free Milk,\
+        2 Tbsp. Plain Fat Free Greek Yogurt,\
+        1/2 Tbsp. Light Mayo,\
+        1 Tbsp. Dry Ranch Seasoning,\
+        1/8 Tsp. Distilled White Vinegar,\
+        1 1/2 C. Rainbow Slaw Mix",
+    directions: "1. Combine the burger ingredients together with clean hands until well blended and form into five equally sized patties. \
+        2. In the mean time, you can prepare your slaw. Whisk together the milk, yogurt, mayo, and seasoning. Add in the vinegar and allow to sit for 5 minutes before \
+        whisking the mixture again and tossing with the slaw. \
+        3. Grill your burgers with your method of choice until cooked through. Ground turkey should reach an internal temperature of 180 degrees F. \
+        4. Once the burgers are cooked, top each with 1/4 c. slaw and enjoy!",
+    source: "https://theskinnyfork.com/blog/healthified-cowboy-turkey-burgers",
+}
+const recipe4 = {
+    name: "Crockpot Potato Soup",
+    category: "Dinner",
+    image: "images/potatosoup.png",
+    ingredients: "2 pounds Yukon gold potatoes, peeled and diced\
+        1 medium yellow onion, diced\
+        6 garlic cloves, minced\
+        10 slices cooked bacon, diced\
+        1 tablespoon dried parsley\
+        3 cups chicken broth\
+        ½ teaspoon salt, or to taste\
+        ¾ teaspoon black pepper, or to taste\
+        4 tablespoons unsalted butter\
+        1/3 cup all-purpose flour\
+        12 ounces evaporated milk, or half & half, or heavy cream\
+        2 cups shredded sharp cheddar cheese\
+        1/2 cup sour cream\
+        Optional Toppings\
+        chives, extra shredded cheddar cheese and bacon",
+    directions: "1. Add diced potatoes, diced onion, minced garlic, bacon, parsley and chicken broth to the slow cooker. \
+        Season with salt and pepper. Stir and cook for 6-8 hours on Low, or for 3-4 hours on High, until the potatoes are fork tender.\
+        2. Once the soup is cooked, add butter to a medium saucepan over medium heat and melt. Whisk in the flour until completely combined.\
+        3. Slowly and gradually whisk the evaporated milk into the flour mixture. Cook over low heat, stirring occasionally, until mixture thickens and begins to simmer.\
+        4. Immediately add the milk-flour mixture to the potato mixture in the slow cooker and stir to combine. Using a potato masher, mash about 3/4 of the potatoes, leaving some unmashed for texture.\
+        5. Add the shredded cheddar cheese and sour cream to the slow cooker. Stir well to combine. Taste for seasoning, and add more salt and pepper to taste.\
+        6. Cover the slow cooker with the lid and continue cooking the soup on Low for 30 minutes, or on high for 15 minutes.\
+        7, Serve warm, garnished with optional chives, bacon, cheese and/or sour cream.",
+    source: "https://www.theslowroasteditalian.com/slow-cooker-potato-soup-recipe",
+}
+const recipe5 = {
+    name: "Cilantro Lime Chicken",
+    category: "Dinner",
+    image: "images/limechick.png",
+    ingredients: "CHICKEN MARINADE:\
+        1 and 1/2 pounds boneless skinless chicken breasts (can also use thighs), \
+        3/4 cup + 1 tablespoon freshly squeezed orange juice, divided, \
+        1/2 cup + 1 teaspoon olive oil, divided, \
+        1/3 cup + 2 tablespoons freshly squeezed lime juice, divided, \
+        1 and 1/2 teaspoons lime zest, divided, \
+        1 tablespoon honey, \
+        1 teaspoon + 1/4 teaspoon cumin, divided, \
+        2 and 1/2 tablespoons soy sauce, \
+        2 teaspoons minced garlic, \
+        1/2 cup coarsely chopped cilantro, divided, \
+        MANGO AVOCADO SALSA: \
+        1 ripe mango, \
+        1 ripe avocado, \
+        1/4 cup finely diced red onion, \
+        1/4 cup finely chopped red pepper, \
+        1 tablespoon finely chopped jalapeno, optional, \
+        Fine sea salt and freshly cracked pepper, \
+        Optional: \
+        cilantro-lime rice or quinoa",
+    directions: "MARINADE: Whisk together all of the marinade ingredients in a medium-sized bowl: 3/4 cup orange juice, \
+                1/2 cup olive oil, 1/3 cup lime juice, 1 teaspoon lime zest, honey, 1 teaspoon each cumin, soy sauce, garlic, and 1/4 cup cilantro. \
+                Add salt and pepper to taste (I use about 1 teaspoon salt and 1/4 teaspoon pepper). Reserve about 1/2 cup of marinade, and pour the rest into a large resealable bag.\
+        1. Trim the chicken of fat. Pound the breasts to even thickness or slice in half to get evenly sized breasts and place in the bag with the rest of the marinade. \
+        Refrigerate for at least 30 minutes, making sure to flip the bag halfway through. I recommend marinating for 2-8 hours.\
+        COOK CHICKEN: The instructions below are to grill the chicken. To see other methods of cooking chicken, read the portion of the post that is right above this recipe card. \
+        Lightly oil the grill grate or grill ridges on a grill pan or add 1 tablespoon oil to a skillet and then place the marinated chicken on the grill. Discard leftover marinade.\
+        1. Grill for 10-12 minutes or until chicken juices run clear and internal temperature is at 165 degrees F. Flip the chicken halfway and brush it generously with the reserved 1/2 cup marinade.\
+        MANGO SALSA: Take the remaining 1 tablespoon orange juice, remaining 1 teaspoon olive oil, 2 tablespoons lime juice, remaining lime zest, and remaining 1/4 cup cilantro and toss together.\
+        (OPTIONAL: When grilling the chicken, also grill the mango. Brush with oil and grill on one side for about 4-5 minutes at medium-high heat.)\
+        1. Chop the mango and avocado into bite-sized pieces. Add to the salsa along with the finely chopped red onion, red pepper, and jalapeño. Season the salsa to taste with pepper, salt, \
+        and remaining 1/4 teaspoon cumin and stir to mix.\
+        ASSEMBLY: Serve grilled chicken over a bed of cilantro-lime rice or quinoa (if desired -- see recipe notes) and add spoonfuls of the salsa on top. Enjoy immediately!",
+    source: "https://www.chelseasmessyapron.com/cilantro-lime-chicken/",
+}
+
+function displayRecipes() {
+    let r1html = `<h2>${recipe1.name}</h2>` +
+        `<h3 id="category">${recipe1.category}</h3>` + `<img src = ${recipe1.image}>` +
+        `<h3>Ingredients</h3>` +
+        `<p>${recipe1.ingredients}</p>` + `<h3>Directions</h3>` +
+        `<p>${recipe1.directions}</p>` + `<h3>Source</h3>` +
+        `<p>${recipe1.source}</p>`;
+    document.getElementById("ob1").innerHTML = r1html;
+    let r2html = `<h2>${recipe2.name}</h2>` +
+        `<h3 id="category"> ${recipe2.category}</h3>` + `<img src = ${recipe2.image}>` +
+        `<h3>Ingredients</h3>` +
+        `<p>${recipe2.ingredients}</p>` + `<h3>Directions</h3>` +
+        `<p>${recipe2.directions}</p>` + `<h3>Source</h3>` +
+        `<p>${recipe2.source}</p>`;
+    document.getElementById("ob2").innerHTML = r2html;
+    let r3html = `<h2 id="burger">${recipe3.name}</h2>` +
+        `<h3 id="category"> ${ recipe3.category }</h3>` + `<img src = ${recipe3.image}>` +
+        `<h3>Ingredients</h3>` +
+        `<p>${recipe3.ingredients}</p>` + `<h3>Directions</h3>` +
+        `<p>${recipe3.directions}</p>` + `<h3>Source</h3>` +
+        `<p>${recipe3.source}</p>`;
+    document.getElementById("ob3").innerHTML = r3html;
+    let r4html = `<h2>${recipe4.name}</h2>` +
+        `<h3 id="category"> ${ recipe4.category }</h3>` + `<img src = ${recipe4.image}>` +
+        `<h3>Ingredients</h3>` +
+        `<p>${recipe4.ingredients}</p>` + `<h3>Directions</h3>` +
+        `<p>${recipe4.directions}</p>` + `<h3>Source</h3>` +
+        `<p>${recipe4.source}</p>`;
+    document.getElementById("ob4").innerHTML = r4html;
+    let r5html = `<h2>${recipe5.name}</h2>` +
+        `<h3 id="category"> ${ recipe5.category }</h3>` + `<img src = ${recipe5.image}>` +
+        `<h3>Ingredients</h3>` +
+        `<p>${recipe5.ingredients}</p>` + `<h3>Directions</h3>` +
+        `<p>${recipe5.directions}</p>` + `<h3>Source</h3>` +
+        `<p>${recipe5.source}</p>`;
+    document.getElementById("ob5").innerHTML = r5html;
+}
