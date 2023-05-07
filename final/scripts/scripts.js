d3fd1ddf46a0b4b0372bc2d6efc697d6d51ad3a3
Sam Davenport <sdavenport@students.mchenry.edu>
2023-05-06 22:20:21 -0500

    updates

--- final/scripts/scripts.js
+++ final/scripts/scripts.js
@@ -11,14 +11,17 @@ $(document).ready(function() {
             case a:
                 $("#success").append(name + ", thank you for RSVPing! We are so excited to celebrate with you!");
                 $("#name").val("");
+                $("form").hide();
                 break;
             case b:
                 $("#success").append(name + ", thank you for RSVPing! We are so excited to celebrate with you!");
                 $("#name").val("");
+                $("form").hide();
                 break;
             default:
                 $("#success").append("I'm sorry, we weren't able to accept that name, please try again!");
                 $("#name").val("");
+                $("#name").css("background-color", "rgba(115, 80, 92, 1)");
                 break;
         }
 
