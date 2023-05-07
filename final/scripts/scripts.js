da7c477911d83a4512e68d2468df41a6ba13f21f
Sam Davenport <sdavenport@students.mchenry.edu>
2023-05-06 22:05:44 -0500

    updates

--- final/scripts/scripts.js
+++ final/scripts/scripts.js
@@ -3,7 +3,24 @@ $(document).ready(function() {
     $("#submit").click(function() {
         event.preventDefault();
         var name = $("#name").val();
-        $("#success").append(name + ", thank you for RSVPing! We are so excited to celebrate with you!");
+        var a = "Sam"
+        var b = "Katie"
+
+
+        switch (name) {
+            case a:
+                $("#success").append(name + ", thank you for RSVPing! We are so excited to celebrate with you!");
+                $("#name").val("");
+                break;
+            case b:
+                $("#success").append(name + ", thank you for RSVPing! We are so excited to celebrate with you!");
+                $("#name").val("");
+                break;
+            default:
+                $("#success").append("I'm sorry, we weren't able to accept that name, please try again!");
+                $("#name").val("");
+                break;
+        }
 
     });
 });
